509ad82457811a2ccbda573d09f5fb71a08648db
Jakub Laník <kubanecxxx@gmail.com>
2012-08-19 19:06:37 +0200

    don't rewrite if file is same

--- main.c
+++ main.c
@@ -8,13 +8,14 @@ void main(char argc, char *argv[])
 	FILE * fn;
 	FILE * out;
 	char pole[200];
+	char file[200];
 
 	if (argc != 2){
 		printf("nic nebude\n");
 		exit(1);
 	}
 
-	fn = fopen(argv[1],"w");
+	fn = fopen(argv[1],"r");
 	out = popen("git log", "r");
 
 	if (fn == NULL)
@@ -34,6 +35,18 @@ void main(char argc, char *argv[])
 	int i = strlen(pole);
 	pole[i-1] = 0;
 
+	fgets(file,sizeof(file), fn);
+
+	if (strstr(file,pole) != NULL)
+	{
+		printf("same\n");
+		exit(0);
+	}
+
+	fclose(fn);
+	printf("not same\n");
+
+	fn = fopen(argv[1],"w");
 	fprintf(fn,"#define BOARD_NAME \"%s\" \n",pole);
 
 	fclose(fn);
